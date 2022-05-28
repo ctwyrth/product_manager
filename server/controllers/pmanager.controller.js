7a633baac9a50d683eb15150573fec71cacb9b88
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2022-05-28 08:57:37 -0700

    Minor changes.

--- server/controllers/pmanager.controller.js
+++ server/controllers/pmanager.controller.js
@@ -3,31 +3,31 @@ const { Product } = require("../models/pmanager.model");
 module.exports.findAllProducts = (req, res) => {
    Product.find({})
       .then(allProducts => res.json(allProducts))
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
 };
 
 module.exports.findOneSingleProduct = (req, res) => {
    Product.findOne({ _id: req.params.id })
       .then(oneSingleProduct => res.json(oneSingleProduct))
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
 };
 
 module.exports.createNewProduct = (req, res) => {
    Product.create(req.body)
       .then(newlyCreatedProduct => res.json(newlyCreatedProduct))
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
 };
 
 module.exports.updateExistingProduct = (req, res) => {
    Product.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
       .then(updatedProduct => res.json(updatedProduct ))
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
 };
 
 module.exports.deleteAnExistingProduct = (req, res) => {
    Product.deleteOne({ _id: req.params.id })
       .then(result => res.json(result))
-      .catch(err => res.json(err));
+      .catch(err => res.status(400).json(err));
 };
 
 module.exports.index = (request, response) => {
@@ -45,5 +45,5 @@ module.exports.exists = (req, res) => {
       return Product.create(req.body);
    })
    .then(saveResult => res.json(saveResult))
-   .catch(err => res.json(err));
+   .catch(err => res.status(400).json(err));
 }
